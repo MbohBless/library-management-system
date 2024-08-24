f5fc86a1e56536e38b978adf58fbb3ad7d10e0a9
Mboh Bless Pearl <63162575+MbohBless@users.noreply.github.com>
2024-08-24 11:44:14 +0000

    Correcting the implementation

--- server/src/embeddings/serverlessEndpoint.ts
+++ server/src/embeddings/serverlessEndpoint.ts
@@ -2,9 +2,21 @@ import fetch from 'node-fetch';
 
 const getTermEmbeddings = async (query) => {
     const apiKey = process.env.EMBEDDING_KEY;
-    const url = process.env.EMBEDDING_URL+"/api/embeddings";
+   
+    const url = process.env.EMBEDDING_URL;
+    console.log(query)
+    console.log(JSON.stringify({ text: query }),)
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({ text: query }),
+      }).then(async (res)=>await res.json())
 
-    const response = await fetch(url).then((res) => res.json());
+      console.log(`THE RESPO: ${response}`)
+    
 
     return response["embedding"];
 };
